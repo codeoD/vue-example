d9372896eca6eb6a09bed442233b9a3dab5c914a
codeoD <david_lee_w@163.com>
2019-04-15 19:56:38 +0800

    first commit to initialize project

---
+++ src/components/RenderlessToggle.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <toggle v-slot="{on, setOn, setOff, toggle}">
+            <div class="button-group">
+                <div :class="`button ${on ? 'btn-on' : 'btn-off'}`" @click="setOn"><span>On Action</span></div>
+                <div :class="`button ${!on ? 'btn-on' : 'btn-off'}`" @click="setOff"><span>Off Action</span></div>
+                <div class="button green" @click="toggle"><span>Toggle Action</span></div>
+            </div>
+        </toggle>
+    </div>
+</template>
+<script>
+const Toggle = {
+    props: {
+        on: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    data () {
+        return {
+            currentState: this.on
+        }
+    },
+    methods: {
+        setOn: function () {
+            this.currentState = true
+        },
+        setOff: function () {
+            this.currentState = false
+        },
+        toggle: function () {
+            this.currentState = !this.currentState
+        }
+    },
+    render(h) {
+        return this.$scopedSlots.default({
+            on: this.currentState,
+            setOn: this.setOn,
+            setOff: this.setOff,
+            toggle: this.toggle,
+        })
+    },
+}
+export default {
+    components: {
+        Toggle,
+    },
+}
+</script>
+<style lang="scss" scoped>
+.button {
+    display: inline-block;
+}
+.btn-on {
+    color: #fff;
+    background-color: chartreuse;
+}
+.btn-off {
+    color: #fff;
+    background-color: cadetblue;
+}
+.green {
+    color: green;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <renderless-toggle />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import RenderlessToggle from '@/components/RenderlessToggle'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    RenderlessToggle,
+  }
+}
+</script>
